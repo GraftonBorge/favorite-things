26383a570b05ff076a079946c8965c12c2cac227
GraftonBorge <graftonmborge@gmail.com>
2021-03-29 14:38:01 -0700

    bugfix

--- projects/favorite-things/js/scripts.js
+++ projects/favorite-things/js/scripts.js
@@ -3,17 +3,31 @@ $(document).ready(function(){
     let animal = $("input#animal").val();
     let car = $("input#car").val();
     let food = $("input#food").val();
-    const combinedArray = [animal, car, food];
-    const animalArray =[combinedArray.push[0]];
-    console.log(animalArray);
-    // combinedArray.push()
 
-
-    // $(".animal").append(animal);
-    // // $("#animal1").append(combinedArray.concat(0))
-    // // const animalArray = [combinedarray[0]];
-    // console.log(combinedArray);
-    // // console.log(animalArray);
     event.preventDefault();
+    
+    
+    let animalArray=[];
+      // animalArray.push(animal);
+    animalArray.forEach(function(element){
+      $("<li>").html(animal).appendTo(".animal");
+      animalArray.push(element);
+    });
+    // let carArray=[];
+    // (".car").submit(function(){
+    //   car.push(carArray)
+    // })
+    // let foodArray=[];
+    // (".food").submit(function(){
+    //   food.push(foodArray)
+    // })
+
+     
+    animalArray;
+    // carArray;
+    // foodArray;
+    console.log(animalArray);
+    });
+   
   })
-})
+ 
